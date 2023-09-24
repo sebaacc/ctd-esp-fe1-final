6760b5c702c47fce620d0b70132d0febeba048b9
Sebastián-Markoja <sebaacc11@gmail.com>
2023-09-23 23:41:10 -0300

    cambio en reducer addFavorito

--- src/redux/slices/favoritosSlice.ts
+++ src/redux/slices/favoritosSlice.ts
@@ -1,37 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface IFavorito {
-    nombre: string;
-    imagen: string;
-  }
+  nombre: string;
+  imagen: string;
+}
 
 export type favoritosState = {
   listaFavoritos: IFavorito[];
 };
 
 const initialState: favoritosState = {
-    listaFavoritos: [],
+  listaFavoritos: [],
 };
 
 export const favoritosSlice = createSlice({
   name: "favoritos",
   initialState: initialState,
   reducers: {
-    addFavorito: (state, action) => { 
-        state.listaFavoritos = [...state.listaFavoritos, action.payload];
-        },
+    addFavorito: (state, action) => {
+      const favoritoIndex = state.listaFavoritos.findIndex(
+        (item) => item.nombre === action.payload.nombre
+      );
+
+      if (favoritoIndex !== -1) {
+        state.listaFavoritos.splice(favoritoIndex, 1);
+      } else {
+        state.listaFavoritos.push(action.payload);
+      }
+    },
     removeFavorito: (state, action) => {
-        state.listaFavoritos = state.listaFavoritos.filter(
-            (item) => item.nombre !== action.payload.nombre
-          );
-      },
+      state.listaFavoritos = state.listaFavoritos.filter(
+        (item) => item.nombre !== action.payload.nombre
+      );
+    },
     clearFavoritos: (state) => {
       state.listaFavoritos = [];
-    }
+    },
   },
 });
 
-export const { addFavorito, removeFavorito, clearFavoritos } = favoritosSlice.actions;
+export const { addFavorito, removeFavorito, clearFavoritos } =
+  favoritosSlice.actions;
 
 const favoritosReducer = favoritosSlice.reducer;
 
